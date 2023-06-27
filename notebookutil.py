5c6de923e87a18176e2c9b06b662030192b4325c
Grant Johnson <johnson30@llnl.gov>
2023-06-27 15:38:29 -0700

    Add checks for data paths

--- notebookutil.py
+++ notebookutil.py
@@ -21,18 +21,24 @@ def dataset_chooser():
     Once they choose one, following cells can get the path with fc.selected_path
     """
     load_dotenv()
-    path=os.getenv("DATAPATH")
+    # Setup reasonable defaults if the user hasn't created an .env file
+    path=os.path.expanduser('~')
     select_default=False
-    if (os.getenv("DEFAULT_PATH")) != None:
-        path=os.getenv("DEFAULT_PATH")
+    if (os.getenv('DEFAULT_PATH')) != None and os.path.exists(os.getenv('DEFAULT_PATH')):
+        path=os.getenv('DEFAULT_PATH')
         select_default=True
+    elif (os.getenv('DATAPATH')) != None and os.path.exists(os.getenv('DATAPATH')):
+        path=os.getenv('DATAPATH')
+    else:
+        print('Defaulting to your home dir. Check .env file.')
+        
     fc = FileChooser(path=path, select_default=select_default, title='<b>Select Wintap Dataset Path</b>', show_only_dirs=True)
     return fc
 
 
 # Set jupyter options
-pd.set_option("display.max_columns", None)
-pd.set_option("display.max_colwidth", None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
 
 # Set altair options
 alt.data_transformers.disable_max_rows()
