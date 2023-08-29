f8e7de115dc4f2872a2b470fea3a3a2bb8bc09bf
Lindsey Whitehurst <whitehurst1@llnl.gov>
2023-08-29 13:53:09 -0700

    fix a file

--- wintappy/analytics/mitre_car/CAR-2014-05-002.sql
+++ wintappy/analytics/mitre_car/CAR-2014-05-002.sql
@@ -3,7 +3,7 @@ SELECT child.pid_hash AS pid_hash
 FROM process AS child,
     process AS parent
 WHERE
-    parent.pid_hash = child.pid_hash
+    parent.pid_hash = child.parent_pid_hash
     AND parent.process_name = 'services.exe'
     AND child.process_name = 'cmd.exe'
     AND child.daypk = {{ search_day_pk|default(20230501, true) }}
